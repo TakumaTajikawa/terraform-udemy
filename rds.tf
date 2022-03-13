f0f6ad47a8b864981ca90f65a6907cc7365e6feb
Takuma Tajikawa <tajikawa@arsaga.jp>
2022-03-13 21:09:17 +0900

    RDSのパスワード用文字列を生成

--- rds.tf
+++ rds.tf
@@ -43,3 +43,12 @@ resource "aws_db_subnet_group" "mysql_standalone_subnetgroup" {
         Env = var.environment
     }
 }
+
+
+# ----------------------------------------
+# RDS instance
+# ----------------------------------------
+resource "random_string" "db_password" {
+    length = 16
+    special = false
+}
