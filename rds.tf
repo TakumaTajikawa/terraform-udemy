da712216526e12af68dc564b2f158e91a52656bf
Takuma Tajikawa <tajikawa@arsaga.jp>
2022-03-20 00:22:24 +0900

    RDSの設定に関する記述を追加

--- rds.tf
+++ rds.tf
@@ -52,3 +52,62 @@ resource "random_string" "db_password" {
     length = 16
     special = false
 }
+
+resource "aws_db_instance" "mysql_standalone" {
+    engine = "mysql"
+    engine_version = "8.0.20"
+
+    # RDSを識別する名前
+    identifier = "${var.project}-${var.environment}-mysql-standalone"
+
+    username = "admin"
+    password = random_string.db_password.result
+
+    instance_class = "db.t2.micro"
+
+    # ストレージ（単位：GB)
+    allocated_storage = 20
+
+    # 最大拡張ストレージ（単位：GB)
+    max_allocated_storage = 50
+
+    storage_type = "gp2"
+
+    # 暗号化を行うか
+    storage_encrypted = false
+
+    multi_az = false
+    availability_zone = "ap-northeast-1a"
+    db_subnet_group_name = aws_db_subnet_group.mysql_standalone_subnetgroup.name
+
+    vpc_security_group_ids = [ aws_security_group.db_sg.id ]
+    publicly_accessible = false
+    port = 3306
+
+    name = "tastylog"
+    parameter_group_name = aws_db_parameter_group.mysql_standalone_parametergroup.name
+    option_group_name = aws_db_option_group.mysql_standalone_optiongroup.name
+
+    # バックアップを行う時間帯
+    backup_window = "04:00-05:00"
+
+    # 過去何日分をバックアップするか
+    backup_retention_period = 7
+
+    # メンテナンス（アップグレード含む）を行う時間
+    maintenance_window = "Mon:05:00-Mon:08:00"
+    # 自動的にマイナーバージョンをアップグレードするか
+    auto_minor_version_upgrade = false
+
+    # 自動で削除させないか
+    deletion_protection = true
+    skip_final_snapshot = false
+
+    apply_immediately = true
+    
+    tags = {
+        Name = "${var.project}-${var.environment}-mysql-standalone"
+        Project = var.project
+        Env = var.environment
+    }
+}
