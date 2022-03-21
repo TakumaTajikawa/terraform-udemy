940d303ae9a79001aa9013bacadb0185ab5b9f4b
Takuma Tajikawa <tajikawa@arsaga.jp>
2022-03-21 23:21:02 +0900

    Route53のレコードの設定に関する記述を追加

--- route53.tf
+++ route53.tf
@@ -12,4 +12,24 @@ resource "aws_route53_zone" "route53_zone" {
         Project = var.project
         Env = var.environment
     }
-} 
+}
+
+resource "aws_route53_record" "reoute53_record" {
+    zone_id = aws_route53_zone.route53_zone.id
+    name = "dev-elb.${var.domain}"
+
+    # レコードタイプ("A", "CNAME"など)
+    type = "A"
+
+    # トラフィックルーティング先
+    alias {
+        # DNSドメイン名
+        name = aws_lb.alb.dns_name
+
+        # ホストゾーンID
+        zone_id = aws_lb.alb.zone_id
+
+        # ヘルスチェックするか
+        evaluate_target_health = true
+    }
+}
