82a76a4d4900c9e71d80a4541935a1b6d8e53f45
Takuma Tajikawa <tajikawa@arsaga.jp>
2022-03-21 12:06:35 +0900

    コメントを追加

--- iam.tf
+++ iam.tf
@@ -1,9 +1,14 @@
 data "aws_iam_policy_document" "ec2_assume_role" {
     statement {
+        # アクションリスト
         actions = ["sts:AssumeRole"]
 
+        # 関連づけるエンティティ（実態）
         principals {
-            type = "service"
+            # AWS、Serviceなど
+            type = "Service"
+
+            # ARN、サービスURLなど
             identifiers = ["ec2.amazonaws.com"]
         }
     }
